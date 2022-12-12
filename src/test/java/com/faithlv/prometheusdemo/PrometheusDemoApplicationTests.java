5873637ea5401d83108ecc19ffd470db31abd7af
zhengjh <2232425497@qq.com>
2022-12-12 17:27:46 +0800

    PrometheusDemo创建

---
+++ src/test/java/com/faithlv/prometheusdemo/PrometheusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faithlv.prometheusdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
